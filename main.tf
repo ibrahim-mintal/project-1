60e35a5528d740828cba5daab7f92f6e78cc112a
ibrahim-mintal <ibrahim.mintal@gmail.com>
2025-04-22 01:40:03 +0200

    call bastion module in root main

--- main.tf
+++ main.tf
@@ -39,3 +39,13 @@ module "elasticache" {
   cluster_id = "my-redis-cluster"
 }
 
+module "bastion" {
+  source = "./modules/bastion"
+
+  image_id          = var.image_id
+  instance_type     = var.aws_instance_type
+  subnet_id         = module.network.public_subnet1_id
+  availability_zone = "${var.region}a"
+  security_group_ids = [aws_security_group.sg-bastion.id]
+  key_name          = aws_key_pair.bastion_key.key_name
+}
